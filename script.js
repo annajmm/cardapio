ca5068b7c517d9ba3fb8414b4523a0203f5af85c
Anna Julia <annajuliamaia2907@gmail.com>
2026-03-25 20:16:02 -0300

    Adiciona botões

---
+++ script.js
@@ -0,0 +1,5 @@
+//Simulação para adicionar itens ao pedido 
+//Seleciona todos os botões que possuem a classe "Categoria"
+    const botoesAdicionar = document.querySelectorAll
+    ('.adicionar');
+
